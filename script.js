b92b3b20c8e4f68d76b58df3b5d1064c15572abd
sakibhasan-24 <sakibhasan7724@gmail.com>
2023-10-15 00:19:38 +0600

    laoding header of categories

--- script.js
+++ script.js
@@ -7,6 +7,21 @@ URL Format: - https://openapi.programming-hero.com/api/videos/category/${id}
 Example: - https://openapi.programming-hero.com/api/videos/category/1000
 
 */
-fetch(`https://openapi.programming-hero.com/api/videos/category/1000`)
+
+// laoding header categories
+
+const headerUrl = ` https://openapi.programming-hero.com/api/videos/categories
+`;
+fetch(headerUrl)
   .then((res) => res.json())
-  .then((data) => console.log(data.data[3]));
+  .then((data) => getHeaderCategory(data.data));
+
+const categoriesContainer = document.getElementById("categories-container");
+function getHeaderCategory(categories) {
+  categories.forEach((data) => {
+    const allCategory = data.category;
+    const h3 = document.createElement("h3");
+    h3.innerText = `${allCategory}`;
+    categoriesContainer.appendChild(h3);
+  });
+}
